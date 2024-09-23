5a22b7d765af6ba46b7fd48659c3f568bb41b395
shooin13 <shooin13@gmail.com>
2024-09-23 21:36:04 +0300

    Added Constants.swift to store constants

---
+++ ImageFeedThird/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constants {
+  static let accessKey = "sxag6w_pOMqE-cNeFfQiIWSVzRUcJ1M04pDKw7L5gR4"
+  static let secretKey = "p2V7BnQobsI82ci7EMtZkBixDKXuB7JVQ_5gTtIN6S4"
+  static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+  static let accessScope = "public+read_user+write_likes"
+  static let defaultBaseURL: URL = .init(string: "https://api.unsplash.com/")!
+}
