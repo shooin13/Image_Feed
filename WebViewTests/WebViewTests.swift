59bd1fc2bec9f9dd53dd9ed42f3bd2dabf7c0a42
Павел Николаев <shooin13@gmail.com>
2024-12-04 23:05:46 +0300

    - added unit test testCodeFromURL to validate correct extraction of authorization code from URL using AuthHelper

--- WebViewTests/WebViewTests.swift
+++ WebViewTests/WebViewTests.swift
@@ -78,4 +78,19 @@ final class WebViewTests: XCTestCase {
     // then
     XCTAssertEqual(code, "testCode123", "Код из URL должен быть равен testCode123")
   }
+  
+  func testCodeFromURL() {
+    // given
+    let configuration = AuthConfiguration.standard
+    let authHelper = AuthHelper(configuration: configuration)
+    var urlComponents = URLComponents(string: "https://unsplash.com/oauth/authorize/native")!
+    urlComponents.queryItems = [URLQueryItem(name: "code", value: "test code")]
+    let url = urlComponents.url!
+    
+    // when
+    let code = authHelper.code(from: url)
+    
+    // then
+    XCTAssertEqual(code, "test code", "Код из URL должен быть корректно распознан")
+  }
 }
